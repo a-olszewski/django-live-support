eb10fb898506e9bbc6fa34a188192fcc0c19933d
a-olszewski <adriols91@gmail.com>
2016-11-17 12:08:46 +0100

    fixes

--- live_support/forms.py
+++ live_support/forms.py
@@ -5,12 +5,15 @@ from live_support.models import Chat, ChatMessage
 
 class ChatForm(forms.ModelForm):
     details = forms.CharField(widget=forms.Textarea, label=_('Question'))
+
     class Meta:
         model = Chat
-        fields = ('name','details',)
+        fields = ['details']
+
 
 class ChatMessageForm(forms.ModelForm):
     message = forms.CharField()
+
     class Meta:
         model = ChatMessage
-        fields = ('message',)
+        fields = ['message']
